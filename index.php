6b09c2ba5314137ddbce43d8645ee5455a9a737a
MA Web <staal.mikkel@gmail.com>
2020-12-02 15:17:21 +0100

    V 0.1 - HTML Boiler Template

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>ERPSystem</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+        <link rel="stylesheet" href="./assets/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    </head>
+    <body>
+        <body>
+                <div id="main">
+                  <div class="columns">
+                      <div class="column is-narrow" id="menu">
+                        <?php include_once("./incl/menu.html"); ?>
+                      </div>
+                      <div class="column" id="system">
+                        <section class="section">
+                          <div class="container">
+                            <nav class="level">
+                              <div class="level-item has-text-centered">
+                                <div>
+                                  <p class="heading">Your invoices for approval</p>
+                                  <p class="title">3,456</p>
+                                </div>
+                              </div>
+                              <div class="level-item has-text-centered">
+                                <div>
+                                  <p class="heading">Total invoices</p>
+                                  <p class="title">123</p>
+                                </div>
+                              </div>
+
+                            </nav>
+                          </div>
+                        </section>
+                      </div>
+                    </div>
+                </div>
+        </body>
+    </body>
+</html>
