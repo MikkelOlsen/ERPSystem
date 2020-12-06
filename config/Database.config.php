30079ea15c53e60bd2ad90b266d73ac2dd3a774a
MA Web <staal.mikkel@gmail.com>
2020-12-06 15:22:34 +0100

    DB Config to localhost?

--- config/Database.config.php
+++ config/Database.config.php
@@ -1,7 +1,5 @@
 <?php
-
-
-    CONST       _DB_HOST_      = 'eu-cdbr-west-03.cleardb.net',
+    CONST       _DB_HOST_      = 'localhost',
                 _DB_NAME_      = 'heroku_10c337ec6ca316c',
                 _DB_USER_      = 'b1140bd4539d94',
                 _DB_PASSWORD_  = 'dd30aefb',
