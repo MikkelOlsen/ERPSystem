a2696c570f6c3a08d6096c88e194e42009ba8b85
MA Web <staal.mikkel@gmail.com>
2020-12-06 15:30:56 +0100

    Revert DB changes

--- config/Database.config.php
+++ config/Database.config.php
@@ -1,6 +1,8 @@
 <?php
+
     CONST       _DB_HOST_      = 'eu-cdbr-west-03.cleardb.net',
                 _DB_NAME_      = 'heroku_10c337ec6ca316c',
                 _DB_USER_      = 'b1140bd4539d94',
                 _DB_PASSWORD_  = 'dd30aefb',
-                _DB_PORT_      = '3306';
+                _DB_PREFIX_    = '',
+                _MYSQL_ENGINE_ = 'InnoDB';
