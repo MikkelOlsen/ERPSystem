44583a47b24bc4858cfeb67a6ed48575ee4ad86a
MA Web <staal.mikkel@gmail.com>
2020-12-06 15:45:34 +0100

    Trying again

--- view/Frontend/Dashboard.view.php
+++ view/Frontend/Dashboard.view.php
@@ -1,5 +1,5 @@
 <?php
-  echo "Hello World! - Heroku works!</br> TEST </br>";
+  echo "Hello World! - Heroku works!</br> TEST v2 </br>";
 
 $url = parse_url(getenv("mysql://b1140bd4539d94:dd30aefb@eu-cdbr-west-03.cleardb.net/heroku_10c337ec6ca316c?reconnect=true"));
 
