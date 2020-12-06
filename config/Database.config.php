60fd37c66a47a3a594f7f74fb055fa86dca1707c
MA Web <staal.mikkel@gmail.com>
2020-12-06 13:35:21 +0100

    DB Config

--- config/Database.config.php
+++ config/Database.config.php
@@ -1,8 +1,7 @@
 <?php
+$url = parse_url(getenv("mysql://b1140bd4539d94:dd30aefb@eu-cdbr-west-03.cleardb.net/heroku_10c337ec6ca316c?reconnect=true"));
 
-    CONST       _DB_HOST_      = 'eu-cdbr-west-03.cleardb.net',
-                _DB_NAME_      = 'heroku_10c337ec6ca316c',
-                _DB_USER_      = 'b1140bd4539d94',
-                _DB_PASSWORD_  = 'dd30aefb',
-                _DB_PREFIX_    = '',
-                _MYSQL_ENGINE_ = 'InnoDB';
+    CONST       _DB_HOST_      = $url["host"],
+                _DB_NAME_      = $url["user"],
+                _DB_USER_      = $url["pass"],
+                _DB_PASSWORD_  = substr($url["path"], 1);
