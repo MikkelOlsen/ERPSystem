defb5bae836e193fde56364ae39f81e4904bf170
MA Web <staal.mikkel@gmail.com>
2020-12-06 15:26:16 +0100

    New local

--- config/Database.config.php
+++ config/Database.config.php
@@ -1,5 +1,5 @@
 <?php
-    CONST       _DB_HOST_      = 'localhost',
+    CONST       _DB_HOST_      = '127.0.0.1',
                 _DB_NAME_      = 'heroku_10c337ec6ca316c',
                 _DB_USER_      = 'b1140bd4539d94',
                 _DB_PASSWORD_  = 'dd30aefb',
