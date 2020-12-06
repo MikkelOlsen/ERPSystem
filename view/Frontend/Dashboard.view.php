5691282092ac0ac6ad2277f840358de4213f2f6b
MA Web <staal.mikkel@gmail.com>
2020-12-06 16:09:16 +0100

    ClearDB

--- view/Frontend/Dashboard.view.php
+++ view/Frontend/Dashboard.view.php
@@ -1,5 +1,5 @@
 <?php
-  echo "Hello World! - Heroku works!</br> TEST v4 </br>";
+  echo "Hello World! - Heroku works!</br> TEST v5 </br>";
 
 $url = parse_url(getenv("mysql://b84ca7ccb58e60:e465574d@eu-cdbr-west-03.cleardb.net/heroku_d892474fb75fe82?reconnect=true"));
 
