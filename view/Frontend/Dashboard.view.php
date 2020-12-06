7d5386d0bdb18dc6b833b663457148ba504a77f0
MA Web <staal.mikkel@gmail.com>
2020-12-06 15:47:42 +0100

    Testing parse URL

--- view/Frontend/Dashboard.view.php
+++ view/Frontend/Dashboard.view.php
@@ -1,7 +1,7 @@
 <?php
-  echo "Hello World! - Heroku works!</br> TEST v2 </br>";
+  echo "Hello World! - Heroku works!</br> TEST v3 </br>";
 
-$url = parse_url(getenv("mysql://b1140bd4539d94:dd30aefb@eu-cdbr-west-03.cleardb.net/heroku_10c337ec6ca316c?reconnect=true"));
+$url = parse_url(getenv("mysql://b1140bd4539d94:dd30aefb@eu-cdbr-west-03.cleardb.net/heroku_10c337ec6ca316c"));
 
   var_dump($url);
 
