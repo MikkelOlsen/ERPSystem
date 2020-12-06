e332bb9cff926d81eb08b8020847670380a611bc
MA Web <staal.mikkel@gmail.com>
2020-12-06 16:11:59 +0100

    Revert clearDB changes

--- config/Database.config.php
+++ config/Database.config.php
@@ -1,8 +1,8 @@
 <?php
 
     CONST       _DB_HOST_      = 'eu-cdbr-west-03.cleardb.net',
-                _DB_NAME_      = 'heroku_10c337ec6ca316c',
-                _DB_USER_      = 'b1140bd4539d94',
-                _DB_PASSWORD_  = 'dd30aefb',
+                _DB_NAME_      = 'heroku_d892474fb75fe82',
+                _DB_USER_      = 'b84ca7ccb58e60',
+                _DB_PASSWORD_  = 'e465574d',
                 _DB_PREFIX_    = '',
                 _MYSQL_ENGINE_ = 'InnoDB';
