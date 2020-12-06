f31075c2ef61e13a9d360a5f1252155878cb26ee
MA Web <staal.mikkel@gmail.com>
2020-12-06 13:43:17 +0100

    SQL Query test

--- view/Frontend/Dashboard.view.php
+++ view/Frontend/Dashboard.view.php
@@ -4,4 +4,6 @@
 $url = parse_url(getenv("mysql://b1140bd4539d94:dd30aefb@eu-cdbr-west-03.cleardb.net/heroku_10c337ec6ca316c?reconnect=true"));
 
   var_dump($url);
+
+  var_dump(Invoice::getInvoice());
  ?>
