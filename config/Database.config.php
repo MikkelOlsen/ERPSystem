2e87fd2693e961c6c3cbdfb692dc7f710cc72736
MA Web <staal.mikkel@gmail.com>
2020-12-06 13:21:27 +0100

    Heroku db config.

--- config/Database.config.php
+++ config/Database.config.php
@@ -1,8 +1,8 @@
 <?php
 
-    CONST       _DB_HOST_      = 'localhost',
-                _DB_NAME_      = 'erp',
-                _DB_USER_      = 'localhost',
-                _DB_PASSWORD_  = '',
+    CONST       _DB_HOST_      = 'eu-cdbr-west-03.cleardb.net',
+                _DB_NAME_      = 'heroku_10c337ec6ca316c',
+                _DB_USER_      = 'b1140bd4539d94',
+                _DB_PASSWORD_  = 'dd30aefb',
                 _DB_PREFIX_    = '',
                 _MYSQL_ENGINE_ = 'InnoDB';
