a602edfc8e729c388a8eca71bd490ad183054d86
MA Web <staal.mikkel@gmail.com>
2020-12-11 14:03:41 +0100

    Vercel bugfixing

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
     require_once __DIR__ . DIRECTORY_SEPARATOR . 'init.php';
-   // Router::init($_SERVER['REQUEST_URI'], ROUTES);
+    Router::init($_SERVER['REQUEST_URI'], ROUTES);
     //CONST ROOT = __DIR__;
     //require_once ROOT . DS . 'lib' . DS .'Layouts' . DS . Router::$Layout . '.layout.php';
 
