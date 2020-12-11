452c1e4951cf1cd3a08e9786ca5450d73a74874a
MA Web <staal.mikkel@gmail.com>
2020-12-11 14:05:41 +0100

    Server var dump

--- api/index.php
+++ api/index.php
@@ -1,7 +1,10 @@
 <?php
 
     require_once __DIR__ . DIRECTORY_SEPARATOR . 'init.php';
-    Router::init($_SERVER['REQUEST_URI'], ROUTES);
+    echo '<pre>';
+    var_dump($_SERVER);
+    echo '</pre>';
+    //Router::init($_SERVER['REQUEST_URI'], ROUTES);
     //CONST ROOT = __DIR__;
     //require_once ROOT . DS . 'lib' . DS .'Layouts' . DS . Router::$Layout . '.layout.php';
 
