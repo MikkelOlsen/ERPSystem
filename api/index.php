aa975ea5f18bd6abd6d28ff9d8dee84039c5fdff
MA Web <staal.mikkel@gmail.com>
2020-12-11 13:51:13 +0100

    Vercel Test

--- api/index.php
+++ api/index.php
@@ -1,5 +1,8 @@
 <?php
+/*
     require_once __DIR__ . DIRECTORY_SEPARATOR . 'init.php';
     Router::init($_SERVER['REQUEST_URI'], ROUTES);
     CONST ROOT = __DIR__;
     require_once ROOT . DS . 'lib' . DS .'Layouts' . DS . Router::$Layout . '.layout.php';
+    */
+    php_info();
