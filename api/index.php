0f93daae71db293c4149c27a9d17f2fe5b71f701
MA Web <staal.mikkel@gmail.com>
2020-12-11 13:55:44 +0100

    Vercel - index.php

--- api/index.php
+++ api/index.php
@@ -1,8 +1,7 @@
 <?php
-/*
+
     require_once __DIR__ . DIRECTORY_SEPARATOR . 'init.php';
     Router::init($_SERVER['REQUEST_URI'], ROUTES);
     CONST ROOT = __DIR__;
     require_once ROOT . DS . 'lib' . DS .'Layouts' . DS . Router::$Layout . '.layout.php';
-    */  
-    echo 'HELLO!';
+
