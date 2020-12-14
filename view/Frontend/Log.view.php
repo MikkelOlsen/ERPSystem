e01a81af20260ece2610c5955aaffef8f830f39a
MA Web <staal.mikkel@gmail.com>
2020-12-14 22:37:48 +0100

    Log page HTML

--- view/Frontend/Log.view.php
+++ view/Frontend/Log.view.php
@@ -1,8 +1,46 @@
-<table class="table myTable">
-    <thead>
-        <tr>
-            <th>Date / Time</th>
-            <th>Message</th>
-        </tr>
-    </thead>
-</table>
+<div class="columns">
+    <div class="column">
+        <div class="box">
+            <nav class="level">
+                <div class="level-item has-text-centered has-text-primary">
+                    <h3>
+                        Log messages - Status
+                    </h3>
+                </div>
+            </nav>
+            <table class="table myTable">
+                <thead>
+                    <tr>
+                        <th>Date / Time</th>
+                        <th>Message</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="column">
+        <div class="box">
+            <nav class="level">
+                <div class="level-item has-text-centered has-text-danger">
+                    <h3>
+                        Log messages - Error
+                    </h3>
+                </div>
+            </nav>
+            <table class="table myTable">
+                <thead>
+                    <tr>
+                        <th>Date / Time</th>
+                        <th>Message</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
